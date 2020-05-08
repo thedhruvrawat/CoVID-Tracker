9b9620e97f69c7292c578971e9333957107153f3
Dhruv Rawat <dhruv.noida175@gmail.com>
2020-05-09 01:57:52 +0530

    Updates

---
+++ precache-manifest.1226f4e6abebe8ab31021cb8e76e0a4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca77e86e1d3e9efa03c4682ec84280c9",
+    "url": "/CoVID-Tracker/index.html"
+  },
+  {
+    "revision": "29bb31b24cc38b0bfd02",
+    "url": "/CoVID-Tracker/static/css/main.a2cc2da2.chunk.css"
+  },
+  {
+    "revision": "895863a5b991820c7d89",
+    "url": "/CoVID-Tracker/static/js/2.3e891f7c.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/CoVID-Tracker/static/js/2.3e891f7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29bb31b24cc38b0bfd02",
+    "url": "/CoVID-Tracker/static/js/main.2e9c2005.chunk.js"
+  },
+  {
+    "revision": "b027a7270a0b9f2147c9",
+    "url": "/CoVID-Tracker/static/js/runtime-main.9797fd90.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/CoVID-Tracker/static/media/corona.d7265326.png"
+  }
+]);
